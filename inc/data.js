6814a6088360bcbdb312341faee762777fdeed1e
Kristaps Ritins <kritinsdev@gmail.com>
2023-07-27 18:44:28 +0300

    auth token

--- inc/data.js
+++ inc/data.js
@@ -1,9 +1,13 @@
 async function fetchApi(endpoint, queryParams = {}) {
     let url = new URL(`${import.meta.env.VITE_API_URL}/${endpoint}`);
-
     Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]))
 
-    const response = await fetch(url);
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
+            'Content-Type': 'application/json'
+        }
+    });
     return await response.json();
 }
 
